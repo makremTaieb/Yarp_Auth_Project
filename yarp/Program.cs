1608d3dc9a40b9b86aafe4060305020f5e7e1b25
Makrem Taieb <makrem16790@gmail.com>
2025-05-29 22:59:42 +0100

    add a yarp project

--- yarp/Program.cs
+++ yarp/Program.cs
@@ -1,21 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddReverseProxy();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+app.MapReverseProxy();
 app.Run();
